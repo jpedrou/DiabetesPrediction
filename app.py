e36b3c0cf59e476d43e5511a8003320744ae7dff
João Pedro <jpedro.open@gmail.com>
2023-08-28 10:47:42 -0300

    New Update

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ with col1:
     st.write("""# Dataset Used""")
     st.write("The Diabetes prediction dataset is a collection of medical and demographic data from patients, along with their diabetes status (positive or negative). The data includes features such as age, gender, body mass index (BMI), hypertension, heart disease, smoking history, HbA1c level, and blood glucose level. This dataset can be used to build machine learning models to predict diabetes in patients based on their medical history and demographic information. This can be useful for healthcare professionals in identifying patients who may be at risk of developing diabetes and in developing personalized treatment plans. Additionally, the dataset can be used by researchers to explore the relationships between various medical and demographic factors and the likelihood of developing diabetes.")
     st.write(
-        "**Project Documentation - [Github Repository](https://github.com/jpedrou/DiabetesML)**")
+        "**Project Documentation - [Github Repository](https://github.com/jpedrou/DiabetesPrediction)**")
     st.write("**Made by [@jpedrou](https://github.com/jpedrou)**")
 
 
